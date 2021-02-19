246de107cbb5cc0c2a9a52fc61ab186a2eb3d7b8
Oscar <ofgonzalez87@gmail.com>
2021-02-19 14:18:30 -0400

    Commit 106

--- src/HookApp.js
+++ src/HookApp.js
@@ -3,9 +3,7 @@ import React from 'react'
 export const HookApp = () => {
     return (
         <div>
-            <h1>Hola Mundo!</h1>
-
-            <h2>Hola de nuevo temp</h2>
+            <h1>Hola Mundo!</h1> 
         </div> 
     )
 }
