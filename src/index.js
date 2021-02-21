a0e958b02fae353072c593e0074ccad3912d747b
Oscar <ofgonzalez87@gmail.com>
2021-02-20 21:36:44 -0400

    Commit 115

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import ReactDOM from 'react-dom';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 
 
 ReactDOM.render( 
-    <RealExampleRef /> ,
+    <Layout /> ,
   document.getElementById('root')
 );
 
