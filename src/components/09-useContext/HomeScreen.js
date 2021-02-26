9b021d3c08800ec4720e1e1f201205f92ebc6912
Oscar <ofgonzalez87@gmail.com>
2021-02-25 23:05:53 -0400

    Commit 140 CreateContext

--- src/components/09-useContext/HomeScreen.js
+++ src/components/09-useContext/HomeScreen.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 export const HomeScreen = () => {
+  const userContext = useContext(UserContext);
+  console.log(userContext);
   return (
     <div>
       <h1>HomeScreen</h1>
-      <hr/>
+      <hr />
     </div>
-  )
-}
+  );
+};
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
-  return <AppRouter />;
+
+  const user = {
+    id: 1234,
+    name: 'Fernando',
+    email: 'fernado.herrera@gmail.com'
+  }
+  return (
+    <UserContext.Provider value = {user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
