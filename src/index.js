ae93afd769c506b0253a7f22e56faf7edb3bb052
Oscar <ofgonzalez87@gmail.com>
2021-02-25 21:25:54 -0400

    Commit 137 Preparación para Rutas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TodoApp } from "./components/08-useReducer/TodoApp";
+// import { TodoApp } from "./components/08-useReducer/TodoApp";
+import { MainApp } from "./components/09-useContext/MainApp";
 // // import { CallbackHook } from "./components/06-memos/CallbackHook";
 // import { Padre } from "./components/07-tarea-memo/Padre";
 // // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
@@ -14,7 +15,7 @@ import { TodoApp } from "./components/08-useReducer/TodoApp";
 // // import { Memorize } from "./components/06-memos/Memorize";
 // // import { MemoHook } from "./components/06-memos/MemoHook";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
 
 
 // import './components/08-useReducer/intro-reducer'
