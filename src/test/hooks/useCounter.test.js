0331a815877eefcc238a98411794547d02ccb9ef
Oscar <ofgonzalez87@gmail.com>
2021-02-27 07:27:17 -0400

    Commit 146 userCounter test

---
+++ src/test/hooks/useCounter.test.js
@@ -0,0 +1,12 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useCounter } from "../../hooks/useCounter";
+describe("useCounter", () => {
+  test("should return default values", () => {
+    const { result } = renderHook(() => useCounter());
+
+    expect(result.current.counter).toBe(10);
+    expect(typeof result.current.increment).toBe("function");
+    expect(typeof result.current.decrement).toBe("function");
+    expect(typeof result.current.reset).toBe("function");
+  });
+});
