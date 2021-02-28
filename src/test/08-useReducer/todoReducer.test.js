79a3679f13c4a80187f21b46fb0e4cbe6b02d673
Oscar <ofgonzalez87@gmail.com>
2021-02-28 00:17:57 -0400

    Commit 153

--- src/test/08-useReducer/todoReducer.test.js
+++ src/test/08-useReducer/todoReducer.test.js
@@ -1,4 +1,7 @@
-import { todoReducer,newTodo } from "../../components/08-useReducer/todoReducer";
+import {
+  todoReducer,
+  newTodo,
+} from "../../components/08-useReducer/todoReducer";
 import { demoTodos, demoTodos2 } from "../fixtures/demoTodos";
 
 describe("todoReducer", () => {
@@ -9,10 +12,22 @@ describe("todoReducer", () => {
   });
   test("should add todo", () => {
     const action = { type: "add", payload: newTodo };
-    const  state  = todoReducer(demoTodos2,action);
-    console.log(state);
-    expect(state).toEqual([...state],newTodo)
-     
-    // expect(state).toEqual(demoTodos);/
+    const state = todoReducer(demoTodos2, action);
+
+    expect(state).toEqual([...state], newTodo);
+  });
+
+  test("should delete todo", () => {
+    const action = { type: "delete", payload: 1 };
+    const state = todoReducer(demoTodos, action);
+    expect(state.length).toBe(1);
+  });
+
+  test("should toggle", () => {
+    const action = { type: "toggle", payload: 2 };
+    const state = todoReducer(demoTodos, action);
+
+    expect(state[1].done).toBe(true);
+    expect(state[0]).toEqual(demoTodos[0]);
   });
 });
